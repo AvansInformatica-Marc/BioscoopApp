8b8b6a6f61fac9d3f77620c70d7a8a50102a89fb
Marc <marc.j.bouwman@gmail.com>
2018-06-22 22:18:13 +0200

    Add layout and function for showing all movies

--- app/src/main/java/nl/bioscoop/mijnbios/MainActivity.java
+++ app/src/main/java/nl/bioscoop/mijnbios/MainActivity.java
@@ -3,9 +3,43 @@ package nl.bioscoop.mijnbios;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends Activity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    private OkHttpClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        client = new OkHttpClient();
+    }
+
+    private void showAllMovies() {
+        Request request = new Request.Builder().url("").build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(final Call call, IOException e) {
+                // Error
+
+                runOnUiThread(() -> {
+                    // For the example, you can show an error dialog or a toast
+                    // on the main UI thread
+                });
+            }
+
+            @Override public void onResponse(Call call, final Response response) throws IOException {
+                String res = response.body().string();
+
+                // Do something with the response
+            }
+        });
     }
 }
