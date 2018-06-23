6507f55e493beb5032e241b3f7089522b43e8bd1
Marc <marc.j.bouwman@gmail.com>
2018-06-23 18:16:00 +0200

    Add movie title & image in detail activity

--- app/src/main/java/nl/bioscoop/mijnbios/MainActivity.java
+++ app/src/main/java/nl/bioscoop/mijnbios/MainActivity.java
@@ -1,18 +1,23 @@
 package nl.bioscoop.mijnbios;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import nl.bioscoop.mijnbios.model.Movie;
 import nl.bioscoop.mijnbios.utils.DataLoader;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Api api;
     private GridView movieGrid;
     private ArrayList<Movie> movies;
@@ -31,14 +36,19 @@ public class MainActivity extends Activity {
         movieGrid.setAdapter(new MovieAdapter(MainActivity.this, movies));
         movieGrid.setOnItemClickListener((adapterView, view, i, l) -> {
             Movie movie = ((MovieAdapter) movieGrid.getAdapter()).getItem(i);
-            Intent intent = new Intent(MainActivity.this, MovieDetailActivity.class);
-            intent.putExtra("Movie", movie);
-            startActivity(intent);
+            if(movie != null) showMovieDetail(movie, view.findViewById(R.id.poster), view.findViewById(R.id.title));
         });
 
         api = new Api(new DataLoader(this));
 
         showAllMovies();
+    }
+
+    private void showMovieDetail(@NonNull Movie movie, @NonNull ImageView poster, @NonNull TextView title){
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, Pair.create(poster, "moviePosterTransition"), Pair.create(title, "movieTitleTransition"));
+        Intent intent = new Intent(MainActivity.this, MovieDetailActivity.class);
+        intent.putExtra("Movie", movie);
+        startActivity(intent, options.toBundle());
     }
 
     private void showAllMovies(){
--- app/src/main/java/nl/bioscoop/mijnbios/MovieDetailActivity.java
+++ app/src/main/java/nl/bioscoop/mijnbios/MovieDetailActivity.java
@@ -1,12 +1,51 @@
 package nl.bioscoop.mijnbios;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MovieDetailActivity extends Activity {
+import com.squareup.picasso.Picasso;
+
+import nl.bioscoop.mijnbios.model.Movie;
+
+public class MovieDetailActivity extends AppCompatActivity {
     @Override @CallSuper protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        View view = toolbar.getChildAt(0);
+        if(view instanceof AppCompatTextView) view.setTransitionName("movieTitleTransition");
+
+        loadActionbar();
+
+        Intent intent = getIntent();
+        @Nullable Movie movie = (Movie) intent.getSerializableExtra("Movie");
+        if(movie != null) loadMovieData(movie);
+    }
+
+    private void loadActionbar(){
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public void loadMovieData(@NonNull Movie movie){
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) actionBar.setTitle(movie.getTitle());
+
+        Picasso.with(this).load(movie.getPoster()).into((ImageView) findViewById(R.id.actionBarImage));
     }
 }
