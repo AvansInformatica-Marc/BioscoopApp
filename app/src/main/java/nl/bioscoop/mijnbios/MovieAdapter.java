d47762387dcdd445148af59fda1d4a41a86bbf49
Marc <marc.j.bouwman@gmail.com>
2018-06-23 11:20:22 +0200

    Update GUI

--- app/src/main/java/nl/bioscoop/mijnbios/MovieAdapter.java
+++ app/src/main/java/nl/bioscoop/mijnbios/MovieAdapter.java
@@ -1,7 +1,6 @@
 package nl.bioscoop.mijnbios;
 
 import android.content.Context;
-import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
