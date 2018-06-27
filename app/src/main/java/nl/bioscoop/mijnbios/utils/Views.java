801d8f25ce0f63757d96b69209a5ced739c100a8
Marc <marc.j.bouwman@gmail.com>
2018-06-27 20:43:41 +0200

    Bugfix

--- app/src/main/java/nl/bioscoop/mijnbios/utils/Views.java
+++ app/src/main/java/nl/bioscoop/mijnbios/utils/Views.java
@@ -1,15 +1,15 @@
 package nl.bioscoop.mijnbios.utils;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Views {
-    @NonNull public static <T extends View> T inflateLayout(@LayoutRes int layout){
-        return inflateLayout(layout, null);
+    @NonNull public static <T extends View> T inflateLayout(@LayoutRes int layout, Context context){
+        return (T) LayoutInflater.from(context).inflate(layout, null, false);
     }
 
     /**
@@ -18,7 +18,7 @@ public class Views {
      * @param parent The future parent of the layout.
      * @return The layout as view.
      */
-    @NonNull public static <T extends View> T inflateLayout(@LayoutRes int layout, @Nullable ViewGroup parent){
+    @NonNull public static <T extends View> T inflateLayout(@LayoutRes int layout, @NonNull ViewGroup parent){
         return (T) LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
     }
 }
