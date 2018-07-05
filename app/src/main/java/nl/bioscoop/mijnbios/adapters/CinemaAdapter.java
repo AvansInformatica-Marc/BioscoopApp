bd378ce40b61235ddfbc72683be7bddcf719a002
Marc <marc.j.bouwman@gmail.com>
2018-07-05 17:23:08 +0200

    Add final functionality

--- app/src/main/java/nl/bioscoop/mijnbios/adapters/CinemaAdapter.java
+++ app/src/main/java/nl/bioscoop/mijnbios/adapters/CinemaAdapter.java
@@ -1,12 +1,16 @@
 package nl.bioscoop.mijnbios.adapters;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,37 @@ public class CinemaAdapter extends ArrayAdapter<Cinema> {
 
             TextView location = view.findViewById(R.id.location);
             location.setText(cinema.getLocation().toLongString(true));
+            location.setOnClickListener((v) -> {
+                try {
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/search/?api=1&query=" + cinema.getLocation().toLongString(false)));
+                    v.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException e){
+                    Toast.makeText(v.getContext(), "Couldn't find app to open this.", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            TextView email = view.findViewById(R.id.email);
+            email.setText(cinema.getEmail());
+            email.setOnClickListener((v) -> {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", cinema.getEmail(), null));
+                    v.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException e){
+                    Toast.makeText(v.getContext(), "Couldn't find app to open this.", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            TextView phone = view.findViewById(R.id.phone);
+            phone.setText(cinema.getPhone());
+            phone.setOnClickListener((v) -> {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + cinema.getPhone()));
+                    v.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException e){
+                    Toast.makeText(v.getContext(), "Couldn't find app to open this.", Toast.LENGTH_LONG).show();
+                }
+            });
         }
 
         return view;
